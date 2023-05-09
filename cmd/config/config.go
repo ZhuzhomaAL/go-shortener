9290a7d7ce63fa7b495c94f3ad211b6f994ccb3e
JuzhomaAL <aljona.zhuzhoma@vseinstrumenti.ru>
2023-05-10 00:23:04 +0300

    iter5-env-added

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -13,4 +14,12 @@ func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&FlagShortAddr, "b", "http://localhost:8080", "address and port before short url")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		FlagShortAddr = envBaseURL
+	}
 }
